1244389ac5ace2b7315339414555afc2ad52ef7e
kantawitpo63 <kantawit.po63@snru.ac.th>
2021-03-02 14:47:24 +0700

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=d00916afe3173de86b34&state=5456c9ec-929e-4524-af05-2a24fb49011d

--- .github/เวิร์กโฟลว์/Flappy01.py
+++ .github/เวิร์กโฟลว์/Flappy01.py
@@ -28,3 +28,77 @@ flap_sound = pygame.mixer.Sound('sound/sfx_wing.wav')
 death_sound = pygame.mixer.Sound('sound/sfx_hit.wav')
 score_sound = pygame.mixer.Sound('sound/sfx_point.wav')
 score_sound_countdown = 100
+def create_pipe():
+	random_pipe_pos = random.choice(pipe_height)
+	bottom_pipe = pipe_surface.get_rect(midtop = (500,random_pipe_pos))
+	top_pipe = pipe_surface.get_rect(midbottom = (500,random_pipe_pos - 400))
+	return bottom_pipe,top_pipe
+
+def move_pipes(pipes):
+	for pipe in pipes:
+		pipe.centerx -= 5
+	return pipes
+
+def draw_pipes(pipes):
+	for pipe in pipes:
+		if pipe.bottom >= 250:
+			screen.blit(pipe_surface,pipe)
+		else:
+			flip_pipe = pygame.transform.flip(pipe_surface,False,True)
+			screen.blit(flip_pipe,pipe)
+def remove_pipes(pipes):
+	for pipe in pipes:
+		if pipe.centerx == -500:
+			pipes.remove(pipe)
+	return pipes
+def check_collision(pipes):
+	for pipe in pipes:
+		if bird_rect.colliderect(pipe):
+			death_sound.play()
+			return False
+
+	if bird_rect.top <= -100 or bird_rect.bottom >= 1000:
+		return False
+
+	return True
+
+def rotate_bird(bird):
+	new_bird = pygame.transform.rotozoom(bird,-bird_movement * 3,1)
+	return new_bird
+
+def bird_animation():
+	new_bird = bird_frames[bird_index]
+	new_bird_rect = new_bird.get_rect(center = (90,bird_rect.centery))
+	return new_bird,new_bird_rect
+
+def score_display(game_state):
+	if game_state == 'main_game':
+		score_surface = game_font.render(str(int(score)),True,(255,255,255))
+		score_rect = score_surface.get_rect(center = (240,100))
+		screen.blit(score_surface,score_rect)
+	if game_state == 'game_over':
+		score_surface = game_font.render(f'Score: {int(score)}' ,True,(255,255,255))
+		score_rect = score_surface.get_rect(center = (240,100))
+		screen.blit(score_surface,score_rect)
+
+		high_score_surface = game_font.render(f'High score: {int(high_score)}',True,(255,255,255))
+		high_score_rect = high_score_surface.get_rect(center = (240,600))
+		screen.blit(high_score_surface,high_score_rect)
+
+def update_score(score, high_score):
+	if score > high_score:
+		high_score = score
+	return high_score
+
+pygame.mixer.pre_init(frequency = 44100, size = 16, channels = 1, buffer = 400)
+pygame.init()
+screen = pygame.display.set_mode((480,800))
+clock = pygame.time.Clock()
+game_font = pygame.font.Font('04B_19.TTF',30)
+
+# Game Variables
+gravity = 0.25
+bird_movement = 0
+game_active = True
+score = 0
+high_score = 0
